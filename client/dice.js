0e6ef8eb27b4d0132c720cd5c5c2a7c15644dded
Eric Feuerstein <esteinsci@gmail.com>
2024-06-15 22:39:43 -0700

    Fix mesh loading for armor dice
    Using custom event for more responsive loading than simple timeout

--- client/dice.js
+++ client/dice.js
@@ -1,44 +1,16 @@
-import * as THREE from 'three';
 import * as CANNON from 'cannon-es';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const loader = new GLTFLoader();
-let armorDiceMesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshStandardMaterial({ color: 0x55555 })
-);
+let armorDiceMesh = null;
 
 loader.load('./models/armor-dice.glb', function (gltf) {
   armorDiceMesh = gltf.scene.children[0];
-  console.log('loaded');
+  window.dispatchEvent(new Event('loadedArmorDiceMesh'));
 });
 
 export function ArmorDice() {
-  this.mesh = null;
-  this.body = null;
-  this.load = () =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        console.log('timed');
-        if (armorDiceMesh != null) {
-          this.mesh = armorDiceMesh;
-          this.body = new CANNON.Body({
-            mass: 1,
-            shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
-            sleepTimeLimit: 0.1,
-          });
-          this.body.position.copy(this.mesh.position);
-          this.body.quaternion.copy(this.mesh.quaternion);
-          this.body.addEventListener('sleep', printTopFaceOfDice);
-
-          resolve();
-        } else {
-          reject('mesh failed to load');
-        }
-      }, 1000);
-    });
-
-  function getTopFaceofDice(euler) {
+  const getTopFaceofDice = (euler) => {
     const eps = 0.1;
     let isZero = (angle) => Math.abs(angle) < eps;
     let isHalfPi = (angle) => Math.abs(angle - 0.5 * Math.PI) < eps;
@@ -79,9 +51,9 @@ export function ArmorDice() {
     }
 
     return null;
-  }
+  };
 
-  function printTopFaceOfDice() {
+  const printTopFaceOfDice = () => {
     this.body.allowSleep = false;
 
     const euler = new CANNON.Vec3();
@@ -93,5 +65,35 @@ export function ArmorDice() {
       return;
     }
     console.log('landed on ' + topFace);
-  }
+  };
+
+  this.mesh = null;
+  this.body = null;
+  this.load = () =>
+    new Promise((resolve) => {
+      const initializeMeshAndBody = () => {
+        window.removeEventListener(
+          'loadedArmorDiceMesh',
+          initializeMeshAndBody
+        );
+
+        this.mesh = armorDiceMesh;
+        this.body = new CANNON.Body({
+          mass: 1,
+          shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
+          sleepTimeLimit: 0.1,
+        });
+        this.body.position.copy(this.mesh.position);
+        this.body.quaternion.copy(this.mesh.quaternion);
+        this.body.addEventListener('sleep', printTopFaceOfDice);
+
+        resolve();
+      };
+
+      if (armorDiceMesh != null) {
+        initializeMeshAndBody();
+      } else {
+        window.addEventListener('loadedArmorDiceMesh', initializeMeshAndBody);
+      }
+    });
 }
