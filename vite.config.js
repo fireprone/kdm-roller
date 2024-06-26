02787e9548d37b808a04bc5d345461cccedb1591
Eric Feuerstein <esteinsci@gmail.com>
2024-06-25 19:55:57 -0700

    add-network: Template out (un-finished) UI

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,9 +11,6 @@ export default defineConfig({
         secure: false,
         ws: true,
       },
-    },
-    hmr: {
-      clientPort: 443,
     },
   },
 });
